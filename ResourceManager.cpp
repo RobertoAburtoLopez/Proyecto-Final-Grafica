089f3af07bc2143f6522cb1c90050617a93fe5a5
José Arturo Ciriaco Vargas <arturcv.puppy1@gmail.com>
2025-10-20 21:56:00 -0600

    Se hace la base del proyecto
    Se agregaron los modelos de los personajes principales, asi los modelos de las piramides y cambio en las texturas del skybox, ademas; de los archivos ResourceManager.h y ResourceManager.cpp para el manejo de los modelos y texturas dentro del main
    
    Co-Authored-By: robaburloks <90802243+RobertoAburtoLopez@users.noreply.github.com>
    Co-Authored-By: Michelle Barrios <143279616+milliyx@users.noreply.github.com>
    Co-Authored-By: Jimena Hernandez Garcia  <72419649+jimenahg5@users.noreply.github.com>

---
+++ ResourceManager.cpp
@@ -0,0 +1,38 @@
+#include "ResourceManager.h"
+
+ResourceManager::ResourceManager() {
+
+    // Skybox
+    std::vector<std::string> blueSky;
+    blueSky.push_back("Texturas/Skybox/blueSky/derecha.png");		// right (voltear verticalmente)
+    blueSky.push_back("Texturas/Skybox/blueSky/izquierda.png");		// left
+    blueSky.push_back("Texturas/Skybox/blueSky/abajo.png");			// bottom
+    blueSky.push_back("Texturas/Skybox/blueSky/arriba.png");		// up (girar 90° en sentido antihorario)
+    blueSky.push_back("Texturas/Skybox/blueSky/atras.png");			// back
+    blueSky.push_back("Texturas/Skybox/blueSky/enfrente.png");		// front
+    skybox = Skybox(blueSky);
+
+    // Texturas
+    brickTexture = Texture("Texturas/ladrillo_rojo.png");   brickTexture.LoadTextureA();
+    dirtTexture = Texture("Texturas/suelo_seco.png");     dirtTexture.LoadTextureA();
+    plainTexture = Texture("Texturas/plain.png");   plainTexture.LoadTextureA();
+    pisoTexture = Texture("Texturas/pasto.png");    pisoTexture.LoadTextureA();
+
+    // Modelos Principales
+    Chicken_Little.LoadModel("Modelos/Chicken Little/Chicken Little.obj");
+    Chilly_Willy.LoadModel("Modelos/Chilly Willy/Chilly Willy.obj");
+    Rikoche.LoadModel("Modelos/Rikoche/Rikoche.obj");
+    Totoro.LoadModel("Modelos/Totoro/Totoro.obj");
+    Totoro_mediano.LoadModel("Modelos/Totoro/Totoro_mediano.obj");
+    Totoro_chiquito.LoadModel("Modelos/Totoro/Totoro_chiquito.obj");
+
+    // Modelos del Zoo
+    animales.LoadModel("Modelos/Zoo/.obj");
+
+    // Modelos de Estructuras
+    Cuadrilatero.LoadModel("Modelos/Estructuras/Cuadrilatero.obj");
+    PiramideChicken.LoadModel("Modelos/Estructuras/Piramide.obj");
+    PiramideChilly.LoadModel("Modelos/Estructuras/Piramide Chilly Willy.obj");
+    PiramideRikoche.LoadModel("Modelos/Estructuras/Piramide Rikoche.obj");
+    PiramideTotoro.LoadModel("Modelos/Estructuras/Piramide Totoro.obj");
+}
